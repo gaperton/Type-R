5eada07f35e2a80f0add3fcfbe6e745a2fc03be9
Vlad Balin <gaperton@gmail.com>
2016-04-19 23:59:32 -0700

    refactoring

--- src/record/attributes/attribute.js
+++ src/record/attributes/attribute.js
@@ -1,5 +1,118 @@
 
 class Attribute {
+    convert( value ){ return value; }
+
+    // create empty object passing backbone options to constructor...
+    create( options ){ return new this.type(); }
+
+    // optimized general purpose isEqual function for typeless attributes
+    // must be overriden in subclass
+    isChanged( a, b ){
+        return !( a === b || ( a && b && typeof a == 'object' && typeof b == 'object' && _.isEqual( a, b ) ) );
+    }
+
+    // generic clone function for typeless attributes
+    // Must be overriden in sublass
+    clone( value, options ){
+        if( value && typeof value === 'object' ){
+            var proto = Object.getPrototypeOf( value );
+
+            if( proto.clone ){
+                // delegate to object's clone if it exist
+                return value.clone( options );
+            }
+
+            if( options && options.deep && proto === Object.prototype || proto === Array.prototype ){
+                // attempt to deep copy raw objects, assuming they are JSON
+                return JSON.parse( JSON.stringify( value ) );
+            }
+        }
+
+        return value;
+    }
+
+    validate( model, value, name ){}
+
+    toJSON( value, key ){
+        return value && value.toJSON ? value.toJSON() : value;
+    }
+
+    createPropertySpec(){
+        const { name, get } = this;
+
+        return {
+            // call to optimized set function for single argument.
+            set( value ){ setAttribute( this, name, value ); },
+
+            // attach get hook to the getter function, if present
+            get : get ? function(){ return get.call( this, this.attributes[ name ], name ); } :
+                        function(){ return this.attributes[ name ]; }
+        }
+    }
+
+    // automatically generated optimized transform function
+    // do not touch.
+    transform( value ){ return value; }
+    handleChange( next, prev, model ){}
+
+    // delegate user and system events on attribute transform
+    delegateEvents( next, prev, model ){
+        prev && prev.trigger && model.stopListening( prev );
+
+        if( next && next.trigger ){
+            if( this.events ){
+                model.listenTo( next, this.events );
+            }
+        }
+
+        //??? Need different way of handling it
+        trigger3( model, 'replace:' + name, model, next, prev );
+    }
+
+    constructor : function( name, spec ){
+    this.name = name;
+
+    Object.transform( this, spec, function( value, name ){
+        if( name === 'events' && this.events ){
+            return Object.assign( this.events, value );
+        }
+
+        if( name === 'get' ){
+            if( this.get ){
+                value.unshift( this.get );
+            }
+            return chainHooks( value );
+        }
+
+        if( name === 'set' ){
+            if( this.set ){
+                value.push( this.set );
+            }
+            return chainHooks( value );
+        }
+
+        return value;
+    }, this );
+
+    this.initialize( spec );
+
+    // assemble optimized transform function...
+    if( this.cast ){
+        this.transform = this._transform = this.cast;
+    }
+    if( this.set ){
+        this.transform = this._transform = this.cast ? transform.hookAndCast : transform.hook;
+    }
+    if( this.events || this.__events ){
+        this.transform =
+            this._transform ? transform.delegateAndMore : this.delegateEvents;
+    }
+}
+}
+
+
+
+class Attribute {
     constructor( name, options ){
         this.name = name;
         this.deepUpdate = false;
@@ -38,47 +151,3 @@ class Attribute {
     }
 }
 
-class PrimitiveType extends Attribute {
-    // cast and set hook...
-    convert( next ){ return next == null ? next : this.type( next ); }
-
-    isChanged( next, prev ){ return next !== prev; }
-
-    // event management, ownership, hooks, if any...
-    handleChange( next, prev ){}
-}
-
-Number._attribute = String._attribute = Boolean._attribute = PrimitiveType;
-
-class DateType extends Attribute {
-
-}
-
-Date._attribute = DateType;
-
-/*
-    Should be plain object.
- */
-class ObjectType extends Attribute {
-    convert( x ){
-        if( typeof x === x )
-    }
-}
-
-Object._attribute = ObjectType;
-
-/*
-    Should be plain array
- */
-class ArrayType extends Attribute {
-    convert( x ){
-        if( !this.isCompatible( x ) ){
-            typeError( x, ' should be an Array' );
-            return [];
-        }
-
-        return x;
-    }
-}
-
-Array._attribute = ArrayType;
--- src/record/attributes/basicTypes.js
+++ src/record/attributes/basicTypes.js
@@ -1,47 +1,120 @@
-import {Attribute} from './model'
+import { Attribute } from './attribute'
 
-class PrimitiveType extends Attribute {
-    constructor( options ){
-        super( options );
+class ConstructorType extends Attribute {
+    convert( value ) {
+        return value == null || value instanceof this.type ? value : new this.type( value );
     }
-    
-    defaults(){ return this.type(); }
-    convert( next ){ return next == null ? next : this.type( next ); }
-    isChanged( next, prev ){ return next !== prev; }
+
+    clone( value, options ) {
+        // delegate to clone function or deep clone through serialization
+        return value.clone ? value.clone( value, options ) : this.convert( JSON.parse( JSON.stringify( value ) ) );
+    }
 }
 
-// There are three major pipelines 
-// `transform` - starts with `convert`
-// `handleChange` - starts empty
-// `get` - starts with attribute read function. 
+Function.prototype._attribute = ConstructorType;
 
-Attribute.hasOptions({
-    get( fun ){
-        if( this._get ){
-            
-        }
-        return { get : fun };
+// Date Attribute
+// ----------------------
+class DateType extends Attribute {
+    convert( value ) {
+        return value == null || value instanceof Date ? value :
+               new Date( typeof value === 'string' ? parseDate( value ) : value );
     }
-        
-    set( fun ){
-        return {
-            // `transform` pipeline starts with a call to `convert`
-            // functions are combined in the way they transform first argument
-            transform( val, options, prev, model ){
-                if( this.isChanged( val, prev ) ){
-                    const next = fun.call( model, val, this.name );
-                    return next === void 0 ? prev : this.convert( next, options );
-                }
+
+    validate( model, value, name ) {
+        if( isNaN( +value ) ) return name + ' is Invalid Date';
+    }
+
+    toJSON( value ) { return value && value.toJSON(); }
+
+    isChanged( a, b ) { return ( a && +a ) !== ( b && +b ); }
+
+    clone( value ) { return value && new Date( +value ); }
+}
+
+Date._attribute = DateType;
+
+// Primitive Types
+// ----------------
+// Global Mock for missing Integer data type...
+// -------------------------------------
+Integer = function( x ) { return x ? Math.round( x ) : 0; };
+
+class PrimitiveType extends Attribute {
+    create() { return this.type(); }
+
+    toJSON( value ) { return value; }
+
+    convert( value ) { return value == null ? value : this.type( value ); }
+
+    isChanged( a, b ) { return a !== b; }
+
+    clone( value ) { return value; }
+}
+
+Boolean._attribute = String._attribute = PrimitiveType;
+
+class NumericType extends PrimitiveType {
+    validate( model, value, name ) {
+        if( !isFinite( value ) ) {
+            return name + ' is invalid number';
+        }
+    }
+}
+
+Integer._attribute = Number._attribute = NumericType;
+
+// Array Type
+// ---------------
+class ArrayType extends Attribute {
+    toJSON( value ) { return value; }
+
+    convert( value ) {
+        // Fix incompatible constructor behaviour of Array...
+        if( value == null || value instanceof Array ) return value;
+
+        // todo: log an error.
+        return [];
+    }
+}
+
+Array._attribute = ArrayType;
+
+var numericKeys    = [ 1, 4, 5, 6, 7, 10, 11 ],
+    msDatePattern  = /\/Date\(([0-9]+)\)\//,
+    isoDatePattern = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/;
+
+function parseDate( date ) {
+    var msDate, timestamp, struct, minutesOffset = 0;
+
+    if( msDate = msDatePattern.exec( date ) ) {
+        timestamp = Number( msDate[ 1 ] );
+    }
+    else if( ( struct = isoDatePattern.exec( date )) ) {
+        // avoid NaN timestamps caused by undefined values being passed to Date.UTC
+        for( var i = 0, k; ( k = numericKeys[ i ] ); ++i ) {
+            struct[ k ] = +struct[ k ] || 0;
+        }
+
+        // allow undefined days and months
+        struct[ 2 ] = (+struct[ 2 ] || 1) - 1;
+        struct[ 3 ] = +struct[ 3 ] || 1;
+
+        if( struct[ 8 ] !== 'Z' && struct[ 9 ] !== undefined ) {
+            minutesOffset = struct[ 10 ] * 60 + struct[ 11 ];
+
+            if( struct[ 9 ] === '+' ) {
+                minutesOffset = 0 - minutesOffset;
             }
         }
+
+        timestamp =
+            Date.UTC( struct[ 1 ], struct[ 2 ], struct[ 3 ], struct[ 4 ], struct[ 5 ] + minutesOffset, struct[ 6 ],
+                      struct[ 7 ] );
     }
-    
-    events( map ){
-        return {
-            handleChange( next, prev, model ){
-                prev && model.stopListening( prev );
-                next && model.listenTo( next, map );
-            }
-        }
+    else {
+        timestamp = Date.parse( date );
     }
-});
+
+    return timestamp;
+}
