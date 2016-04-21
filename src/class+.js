addc97103abe9fb0f1e2257848476d94f33cfb32
Vlad Balin <gaperton@gmail.com>
2016-04-20 18:43:52 -0700

    detailed design of attribute and basic types

--- src/class+.js
+++ src/class+.js
@@ -4,64 +4,64 @@ const mergeRules = {
     },
 
     pipe( a, b ){
-        return function( x ){
+        return function( x ) {
             return a.call( this, b.call( this, x ) );
         }
     },
 
     sequence( a, b ){
-        return function(){
+        return function() {
             a.apply( this, arguments );
             b.apply( this, arguments );
         }
     },
 
     reverse( a, b ){
-        return function(){
+        return function() {
             b.apply( this, arguments );
             a.apply( this, arguments );
         }
     },
 
     every( a, b ){
-        return function(){
+        return function() {
             return a.apply( this, arguments ) && b.apply( this, arguments );
         }
     },
 
     some( a, b ){
-        return function(){
+        return function() {
             return a.apply( this, arguments ) || b.apply( this, arguments );
         }
     }
 };
 
-export function getPropertyDescriptor( obj, prop ){
-    for( var desc; !desc && obj; obj = Object.getPrototypeOf( obj ) ){
+export function getPropertyDescriptor( obj, prop ) {
+    for( var desc; !desc && obj; obj = Object.getPrototypeOf( obj ) ) {
         desc = Object.getOwnPropertyDescriptor( obj, prop );
     }
 
     return desc;
 }
 
-function mergeProps( target, source, rules = {} ){
+function mergeProps( target, source, rules = {} ) {
     const sourceProps = Object.getOwnPropertyNames( source );
-    for( var i = 0; i < sourceProps.length; i++ ){
+    for( var i = 0; i < sourceProps.length; i++ ) {
         const name       = sourceProps[ i ],
               sourceProp = Object.getOwnPropertyDescriptor( source, name ),
               destProp   = getPropertyDescriptor( target, name ); // Shouldn't be own
 
-        if( destProp ){
+        if( destProp ) {
             const rule  = rules[ name ],
                   value = destProp.value;
 
-            if( rule && value ){
+            if( rule && value ) {
                 target[ name ] = typeof rule === 'object' ?
                                  mergeRules.merge( value, sourceProp.value, rule ) :
                                  mergeRules[ rule ]( value, sourceProp.value );
             }
         }
-        else{
+        else {
             Object.defineProperty( target, name, sourceProp );
         }
     }
@@ -70,15 +70,15 @@ function mergeProps( target, source, rules = {} ){
 }
 
 export class Class {
-    static create( attrs, options ){
+    static create( attrs, options ) {
         return new this( attrs, options );
     }
 
-    static mixins( mixins = [] ){
+    static mixins( mixins = [] ) {
         const proto      = this.prototype,
               mergeRules = this.mixinRules || {};
 
-        for( var i = mixins.length - 1; i >= 0; i-- ){
+        for( var i = mixins.length - 1; i >= 0; i-- ) {
             const mixin = mixins[ i ];
             mergeProps( proto, mixin, mergeRules );
         }
@@ -86,12 +86,12 @@ export class Class {
         return this;
     }
 
-    static define( spec = {} ){
+    static define( spec = {} ) {
         const proto = this.prototype,
               Base  = Object.getPrototypeOf( proto ).constructor;
 
         // Remove abstract class factory on inheritance
-        if( Base.create !== Class.create && Base.create === this.create ){
+        if( Base.create !== Class.create && Base.create === this.create ) {
             this.create = Class.create;
         }
 
@@ -100,7 +100,7 @@ export class Class {
         Object.assign( proto, specProps );
         Object.defineProperties( proto, properties );
 
-        if( Base.mixinRules && Base.mixinRules !== this.mixinRules ){
+        if( Base.mixinRules && Base.mixinRules !== this.mixinRules ) {
             mergeProps( this.mixinRules, Base.mixinRules );
         }
 
@@ -108,28 +108,28 @@ export class Class {
     }
 }
 
-export function define( spec ){
+export function define( spec ) {
     return typeof spec === 'function' ?
-               spec.define() :
-               function( Class ){
-                   Class.define( spec );
-               }
+           spec.define() :
+           function( Class ) {
+               Class.define( spec );
+           }
 }
 
-export function mixins(){
+export function mixins() {
     const arr = Array.prototype.slice.call( arguments );
 
-    return function( Ctor ){
-        if( Ctor.mixins ){
+    return function( Ctor ) {
+        if( Ctor.mixins ) {
             Ctor.mixins( arr );
         }
-        else{
+        else {
             Class.mixins.call( Ctor, arr );
         }
     }
 }
 
-export function attach( Ctor ){
+export function attach( Ctor ) {
     Ctor.create = Class.create;
     Ctor.define = Class.define;
     Ctor.mixins = Class.mixins;
@@ -160,9 +160,9 @@ export let log = {
     }
 };
 
-export function transform( dest, source, fun ){
-    for( var name in source ){
-        if( source.hasOwnProperty( name ) ){
+export function transform( dest, source, fun ) {
+    for( var name in source ) {
+        if( source.hasOwnProperty( name ) ) {
             var value = fun( source[ name ], name );
             typeof value === 'undefined' || ( dest[ name ] = value );
         }
@@ -171,9 +171,9 @@ export function transform( dest, source, fun ){
     return dest;
 }
 
-function forAllArgs( fun ){
-    return function( dest ){
-        for( var i = 1; i < arguments.length; i++ ){
+function forAllArgs( fun ) {
+    return function( dest ) {
+        for( var i = 1; i < arguments.length; i++ ) {
             const source = arguments[ i ];
             source && fun( dest, source );
         }
@@ -182,64 +182,108 @@ function forAllArgs( fun ){
     }
 }
 
-export function fastAssign( dest, source ){
-    for( var name in source ){
+export function fastAssign( dest, source ) {
+    for( var name in source ) {
         dest[ name ] = source[ name ];
     }
 }
 
-export function fastDefaults( dest, source ){
-    for( var name in source ){
+export function fastDefaults( dest, source ) {
+    for( var name in source ) {
         dest[ name ] === void 0 || ( dest[ name ] = source[ name ] );
     }
 }
 
-export const assign = forAllArgs( ( dest, source ) =>{
-    for( var name in source ){
-        if( source.hasOwnProperty( name ) ){
+export const assign = forAllArgs( ( dest, source ) => {
+    for( var name in source ) {
+        if( source.hasOwnProperty( name ) ) {
             dest[ name ] = source[ name ];
         }
     }
-});
+} );
 
-export const defaults = forAllArgs( ( dest, source ) =>{
-    for( var name in source ){
-        if( source.hasOwnProperty( name ) ){
+export const defaults = forAllArgs( ( dest, source ) => {
+    for( var name in source ) {
+        if( source.hasOwnProperty( name ) ) {
             dest[ name ] === void 0 || ( dest[ name ] = source[ name ] );
         }
     }
-});
+} );
 
-export function createForEach( attrSpecs ){
+export function createForEach( attrSpecs ) {
     var statements = [ 'var v;' ];
 
-    for( let name in attrSpecs ){
+    for( let name in attrSpecs ) {
         statements.push( '(v=a.' + name + ')' + '===void 0||f(v,"' + name + '");' );
     }
 
     return new Function( 'a', 'f', statements.join( '' ) );
 }
 
-export function createCloneCtor( attrSpecs ){
+export function createCloneCtor( attrSpecs ) {
     var statements = [];
 
-    for( let name in attrSpecs ){
+    for( let name in attrSpecs ) {
         statements.push( "this." + name + "=x." + name + ";" );
     }
 
-    var CloneCtor = new Function( "x", statements.join( '' ) );
+    var CloneCtor       = new Function( "x", statements.join( '' ) );
     CloneCtor.prototype = Object.prototype;
     return CloneCtor;
 }
 
-export function createTransformCtor( attrSpecs ){
+export function createTransformCtor( attrSpecs ) {
     var statements = [ 'var v;' ];
 
-    for( let name in attrSpecs ){
+    for( let name in attrSpecs ) {
         statements.push( 'this.' + name + '=(v=a.' + name + ')' + '===void 0?void 0 :f(v,"' + name + '");' );
     }
 
-    var TransformCtor = new Function( "a", 'f', statements.join( '' ) );
+    var TransformCtor       = new Function( "a", 'f', statements.join( '' ) );
     TransformCtor.prototype = Object.prototype;
     return TransformCtor;
+}
+
+const ArrayProto = Array.prototype;
+
+export function notEqual( a, b ) {
+    if( a === b ) return false;
+
+    if( a && b && typeof a == 'object' && typeof b == 'object' ) {
+        const protoA = Object.getPrototypeOf( a );
+
+        if( protoA !== Object.getPrototypeOf( b ) ) return true;
+
+        return protoA === ArrayProto ?
+                   arraysNotEqual( a, b ) :
+                   objectsNotEqual( a, b );
+    }
+
+    return true;
+}
+
+function objectsNotEqual( a, b ) {
+    const keysA = Object.keys( a );
+
+    if( keysA.length !== Object.keys( b ).length ) return true;
+
+    for( let i = 0; i < keysA.length; i++ ) {
+        const key = keysA[ i ];
+
+        if( !b.hasOwnProperty( key ) || notEqual( a[ key ], b[ key ] ) ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function arraysNotEqual( a, b ) {
+    if( a.length !== b.length ) return true;
+
+    for( let i = 0; i < a.length; i++ ) {
+        if( notEqual( a[ i ], b[ i ] ) ) return true;
+    }
+
+    return false;
 }
---
+++ src/record/attribute.js
@@ -0,0 +1,179 @@
+import { setAttribute } from './transactions'
+import { notEqual, assign } from '../class+'
+
+export class Attribute {
+    /**
+     * Update pipeline functions
+     * =========================
+     *
+     * Stage 0. canBeUpdated( value )
+     * - presence of this function implies attribute's ability to update in place.
+     *
+     * Stage 1. Transform stage
+     */
+    transform( value ) { return value; }
+
+    // convert attribute type to `this.type`
+    convert( value, options ) { return value; }
+
+    // when set hook is present, call it
+    convertAndSet( value, options, prev, model ) {
+        const next = this.convert( value, options, model );
+
+        if( this.isChanged( next, prev ) ) {
+            const value = this.set.call( model, next, this.name );
+            return value === void 0 ? prev : this.convert( value, options );
+        }
+    }
+
+    /**
+     * Stage 2. Check if attr value is changed
+     */
+    // todo: remove underscore dependency
+    isChanged( a, b ) {
+        return notEqual( a, b );
+    }
+
+    /**
+     * Stage 3. Handle attribute change
+     */
+    handleChange( next, prev, model ) {}
+
+    // delegate user and system events on attribute transform
+    // todo: remove backbone events dependency from the core
+    delegateEvents( next, prev, model ) {
+        // console.assert( this.events );
+        prev && prev.trigger && model.stopListening( prev );
+
+        if( next && next.trigger ) {
+            model.listenTo( next, this.events );
+        }
+    }
+
+    // attribute-level change event.
+    onAttrChange( next, prev, model ) {
+        this.onChange.call( model, next, this.name );
+    }
+
+    /**
+     * End update pipeline definitions.
+     */
+
+    // create empty object passing backbone options to constructor...
+    create( options ) { return new this.type(); }
+
+    // generic clone function for typeless attributes
+    // Must be overriden in sublass
+    clone( value, options = {} ) {
+        if( value && typeof value === 'object' ) {
+            // delegate to object's clone(), if it exist...
+            if( value.clone ) {
+                return value.clone( options );
+            }
+
+            if( options.deep ){
+                const proto = Object.getPrototypeOf( value );
+
+                // attempt to deep copy raw objects, assuming they are JSON
+                if( proto === Object.prototype || proto === Array.prototype ){
+                    return JSON.parse( JSON.stringify( value ) );
+                }
+            }
+        }
+
+        return value;
+    }
+
+    validate( model, value, name ) {}
+
+    toJSON( value, key ) {
+        return value && value.toJSON ? value.toJSON() : value;
+    }
+
+    createPropertyDescriptor() {
+        const { name, getHook } = this;
+
+        return {
+            // call to optimized set function for single argument.
+            set( value ){
+                setAttribute( this, name, value );
+            },
+
+            // attach get hook to the getter function, if present
+            get : getHook ?
+                  function() {
+                      return getHook.call( this, this.attributes[ name ], name );
+                  } :
+                  function() {
+                      return this.attributes[ name ];
+                  }
+        }
+    }
+
+    constructor( name, options ) {
+        const {
+                  value, type, parse, toJSON,
+                  getHooks = [],
+                  transforms = [],
+                  changeHandlers = []
+              } = this.options = options;
+
+        this.name  = name;
+        this.value = value;
+        this.type  = type;
+
+        this.parse  = parse;
+        this.toJSON = toJSON === void 0 ? this.toJSON : toJSON;
+
+        /**
+         * Assemble pipelines...
+         */
+
+        // `convert` is default transform, which is always present...
+        this.addTransform( this.convert );
+
+        // No change handler by default
+        this.changeHandler = null;
+
+        // Get hook from the attribute will be used first...
+        this.getHook = this.get || null;
+
+        // let subclasses configure the pipeline...
+        this.initialize.apply( this, arguments );
+
+        // let attribute spec configure the pipeline...
+        getHooks.forEach( gh => this.addGetHook( gh ) );
+        transforms.forEach( t => this.addTransform( t ) );
+        changeHandlers.forEach( ch => this.addChangeHandler( ch ) );
+    }
+
+    addGetHook( next ) {
+        const { prev } = this;
+
+        this.getHook = prev ?
+                       function( value, name ) {
+                           const next = prev.call( value, name );
+                           return next.call( next, name );
+                       } : next;
+    }
+
+    addTransform( next ) {
+        const { prev } = this;
+
+        this.transform = prev ?
+                         function( value, options, prev, model ) {
+                             const next = prev.call( this, value, options, prev, model );
+                             return next.call( this, next, options, prev, model );
+                         } : next;
+    }
+
+    addChangeHandler( next ) {
+        const { prev } = this;
+
+        this.handleChange = prev ?
+                            function( next, prev, model ) {
+                                prev.call( this, next, prev, model );
+                                next.call( this, next, prev, model );
+                            } : next;
+    }
+}
