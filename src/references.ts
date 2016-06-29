f604f6978ebc4b9ab4042e94afd8f7a5187ed207
Vlad Balin <gaperton@gmail.com>
2016-06-29 16:45:37 -0400

    refactoring

--- src/references.ts
+++ src/references.ts
@@ -10,12 +10,12 @@
 const referenceMask =  /\~|\^|([^.]+)/g;
 
 // Need to be implemented in the model
-function get( object : any, reference : string ) : any { 
-    return resolve( object, reference, ( self, key ) => self[ key ] );
+export function resolve( reference : string, object : any ) : any { 
+    return parse( object, reference, ( self, key ) => self[ key ] );
 }
 
 // Compile reference to function
-function compile( reference : string ) : ( root : any ) => any {
+export function compile( reference : string ) : ( root : any ) => any {
     const path = reference
                     .match( referenceMask )
                     .map( key => key === '~' ? 'getStrore()' : ( key === '^' ? 'getOwner()' : key ) )
@@ -24,7 +24,7 @@ function compile( reference : string ) : ( root : any ) => any {
     return <any> new Function( 'self', `return self.${ path };` );          
 }
 
-function resolve( object : any, reference : string, action ) : any {
+function parse( object : any, reference : string, action ) : any {
     const path = reference.match( referenceMask ),
           skip = path.length - 1;
     
