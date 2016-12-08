f8de444908958907cf26fb90058c6db506e3bdf1
Vlad Balin <gaperton@gmail.com>
2016-12-08 11:06:21 -0500

    more events performance tests

--- tests/nestedtypes/performance/complex.js
+++ tests/nestedtypes/performance/complex.js
@@ -31,6 +31,71 @@ define( function( require, exports, module ){
             watcher(){ this._counter++; }
         });
 
+        describe( 'five lisneners', function(){
+            it( 'simple on/off', function(){
+                var source = new FlatModel(),
+                    d1 = new FlatModel(), d2 = new FlatModel(),
+                    d3 = new FlatModel(), d4 = new FlatModel(), d5 = new FlatModel();
+
+                function callback(){ this._counter++; }
+
+                for( var i = 0; i < 1000000; i++ ){
+                    source.on( 'change', callback, d1 )
+                        .on( 'change', callback, d2 )
+                        .on( 'change', callback, d3 )
+                        .on( 'change', callback, d4 )
+                        .on( 'change', callback, d5 );
+
+                    source.off( 'change', callback, d1 )
+                        .off( 'change', callback, d2 )
+                        .off( 'change', callback, d3 )
+                        .off( 'change', callback, d4 )
+                        .off( 'change', callback, d5 );
+                }
+            });
+
+            it( 'trigger', function(){
+                var source = new FlatModel(),
+                    d1 = new FlatModel(), d2 = new FlatModel(),
+                    d3 = new FlatModel(), d4 = new FlatModel(), d5 = new FlatModel();
+
+                function callback(){ this._counter++; }
+
+                source.on( 'change', callback, d1 )
+                        .on( 'change', callback, d2 )
+                        .on( 'change', callback, d3 )
+                        .on( 'change', callback, d4 )
+                        .on( 'change', callback, d5 );
+
+                for( var i = 0; i < 1000000; i++ ){
+                    source.trigger( 'change', source, {} );
+                }
+            });
+
+            it( 'listentTo/stopListening', function(){
+                var source = new FlatModel(),
+                    d1 = new FlatModel(), d2 = new FlatModel(),
+                    d3 = new FlatModel(), d4 = new FlatModel(), d5 = new FlatModel();
+
+                function callback(){ this._counter++; }
+
+                for( var i = 0; i < 1000000; i++ ){
+                    d1.listenTo( source, 'change', callback );
+                    d2.listenTo( source, 'change', callback );
+                    d3.listenTo( source, 'change', callback );
+                    d4.listenTo( source, 'change', callback );
+                    d5.listenTo( source, 'change', callback );
+
+                    d1.stopListening( source );
+                    d2.stopListening( source );
+                    d3.stopListening( source );
+                    d4.stopListening( source );
+                    d5.stopListening( source );
+                }
+            });
+
+        } );
+
         describe( 'Watchers', function(){
             it( 'Create and dispose 500K models', function(){
                 for( var i = 0; i < 500000; i++ ){
