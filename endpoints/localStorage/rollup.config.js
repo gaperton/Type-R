c54a8f8592668a82933c8c59ea98086f9f9cc9d1
Vlad Balin <gaperton@gmail.com>
2017-11-06 13:19:50 -0500

    clean up

--- endpoints/localStorage/rollup.config.js
+++ endpoints/localStorage/rollup.config.js
@@ -3,12 +3,12 @@ import uglify from 'rollup-plugin-uglify';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
-    input : 'lib/endpoints/memory.js',
+    input : 'lib/endpoints/localStorage.js',
 
     output : {
-        file   : 'endpoints/memory/index.js',
+        file   : 'endpoints/localStorage/index.js',
         format : 'umd',
-        name   : 'memoryIO'
+        name   : 'localStorageIO'
     },
     plugins: [
         resolve(), //for support of `import X from "directory"` rather than verbose `import X from "directory/index"`
--- lib/endpoints/fakeEndpoint.d.ts
+++
@@ -1,7 +0,0 @@
-import { IOPromise } from '../io-tools';
-export default abstract class FakeEndpoint {
-    delay: number;
-    constructor(delay: number);
-    resolve(value: any): IOPromise<any>;
-    reject(value: any): IOPromise<any>;
-}
