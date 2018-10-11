6b3dc1bdea79ff7a92e249b59aa14f14ea6ea3ab
Vlad Balin <gaperton@gmail.com>
2018-10-11 16:19:31 -0400

    Removed all tests folder

--- tests.old/README.md
+++
@@ -1,3 +0,0 @@
-Old regression tests from `nestedtypes` and new tests from `typescript` are being executed automatically during the build.
-
-Add new tests to `typescript` folder.
--- tests.old/typescript/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./advanced");
-require("./mixins.test");
-require("./primitives");
-require("./reported-bugs");
-require("./io");
-require("./ext-types");
-//# sourceMappingURL=index.js.map
