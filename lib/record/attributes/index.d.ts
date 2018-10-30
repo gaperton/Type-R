a380260f00dacd9766089a4cfb78fd8c634a7468
Vlad Balin <gaperton@gmail.com>
2018-10-30 18:54:36 -0400

    Removed old lib files

--- lib/record/attributes/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './any';
-export * from './basic';
-export * from './date';
-export * from './owned';
-export * from './shared';
-export declare function getMetatype(Ctor: Function): any;
