b43d0283de5a8175148cce3ea33e0aae368b7bd1
Vlad Balin <gaperton@gmail.com>
2018-12-01 17:38:31 -0500

    typings

--- ext-types/lib/email.d.ts
+++ ext-types/lib/email.d.ts
@@ -1,2 +1,2 @@
 export declare function isEmail(x: string): boolean;
-export declare const Email: import("../../lib/record/attrDef").ChainableAttributeSpec;
+export declare const Email: import("../../lib").ChainableAttributeSpec;
